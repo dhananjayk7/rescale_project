f8c4dcb60d82bffb2fc11dd1e88ec7936a8fdbf5
Dhananjay Kulkarni <dhananjaykulkarni@quickenloans.com>
2018-11-12 21:32:45 -0500

    refactoring code

--- modules/testrescale-072de1f0-3fbf-4bce-a86e-72b974853f87/lambda_function.py
+++ modules/testrescale-072de1f0-3fbf-4bce-a86e-72b974853f87/lambda_function.py
@@ -7,21 +7,21 @@ from dateutil import tz
 
 def lambda_handler(event, context):
 
+    #Source S3 bucket.
     buck_name = "sample-spot-prices"
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(buck_name)
-    
-    #today = datetime.datetime.today().strftime('%Y%m%d')
+
     today = datetime.today()
     today_date = get_current_date(today)
-
-    #print(today_date)
     s3upload = boto3.client('s3')
     uploadbucketname = "rescale"
     cnt = 1
     for o in bucket.objects.all():
         key = o.key
         obj = s3.Object(buck_name,key)
+
+        #parsing the accountid, date and region from the file name
         a = obj.get()['Body'].read()
         a = json.loads(a)
         b = key.split(".")
@@ -29,11 +29,13 @@ def lambda_handler(event, context):
         fdate= fnamelist[1]
         faccountid = fnamelist[2]
         fregion = fnamelist[3]
+
+        # reading only files added on the same day
         if str(fdate) == str(today_date):
             print("true")
             s3filekey = fdate+'test'+str(cnt)+'.csv'
+
             #json to csv
-            #f = csv.writer(open(fdate+'test'+str(cnt)+'.csv','w'), lineterminator='\n')
             f= open('/tmp/'+s3filekey,'w',newline='')
             writer = csv.writer(f)
             writer.writerow(["AccountID", "Region","AvailabilityZone","InstanceType","ProductDescription","SpotPrice","Timestamp"])
@@ -46,19 +48,20 @@ def lambda_handler(event, context):
                             x["ProductDescription"],
                             x["SpotPrice"],
                             x["Timestamp"]])
-    
             f.close()
+
+            #uploading csvv files to new S3 bucket
             s3upload.upload_file('/tmp/'+s3filekey,uploadbucketname,s3filekey)
             
         else:
-            #print("true")
             continue;
+
     return {
         'statusCode': 200,
         'body': json.dumps('Successfully wrote files to S3')
     }
 
-
+# UTC to EST
 def get_current_date(today):
     from_zone = tz.tzutc()
     to_zone = tz.gettz('America/New_York')
